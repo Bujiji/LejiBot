29240029c4023c2137a65a192c343114c3e34b74
ciju <s0506140@taivs.tp.edu.tw>
2018-11-30 22:24:58 +0800

    test1

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -2,11 +2,6 @@ class KamigoController < ApplicationController
 	require 'line/bot'
 	protect_from_forgery with: :null_session
 	def webhook
- 			 # Line Bot API 物件初始化
-			client = Line::Bot::Client.new { |config|
-		    config.channel_secret = '85856371f291d9fc9c6013a0d0664fe6'
-    		config.channel_token = 'lU4bS1RLsfyHC91MyKiMGDxgKu5bTtkoSd+dzoAGAuoePgWN7ecrMZ1SdojLgP3QFUWU7ufoplhcwM41L96uJgUIzh7OxVK1ddu+udNWnU1kH3CYC03p8NsLntbM0fBEOcp1cApdIJH29GjnqM6rhwdB04t89/1O/w1cDnyilFU='
-  }
   
   			# 取得 reply token
   			reply_token = params['events'][0]['replyToken']
@@ -20,13 +15,20 @@ class KamigoController < ApplicationController
   			}
 
  			# 傳送訊息
-  			response = client.reply_message(reply_token, message)
+  			response = line.reply_message(reply_token, message)
     
   			# 回應 200
   			head :ok
 	end 
-	def test
-		p test
+	def line
+				 # Line Bot API 物件初始化
+			@line ||= Line::Bot::Client.new { |config|
+		    config.channel_secret = '85856371f291d9fc9c6013a0d0664fe6'
+    		config.channel_token = 'lU4bS1RLsfyHC91MyKiMGDxgKu5bTtkoSd+dzoAGAuoePgWN7ecrMZ1SdojLgP3QFUWU7ufoplhcwM41L96uJgUIzh7OxVK1ddu+udNWnU1kH3CYC03p8NsLntbM0fBEOcp1cApdIJH29GjnqM6rhwdB04t89/1O/w1cDnyilFU='
+  }
+    end
+	def test.
+		return 't'
 	end
 
 	def eat
